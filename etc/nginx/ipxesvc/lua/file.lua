db2cc6e238d9593e4db0fa2254412b9f6e3008b6
TechHypno <techhypno.com@gmail.com>
2023-03-29 15:51:51 +0200

    cleanup

--- etc/nginx/ipxesvc/lua/file.lua
+++ etc/nginx/ipxesvc/lua/file.lua
@@ -4,12 +4,6 @@ local client_requested_file = ngx.var[1]
 local files_dir = '/userdata/files'
 local file_path = files_dir..'/'..client_requested_file
 
--- local pfile = io.popen('ls"'..file_path..'"')
--- for filename in pfile:lines() do
---     ngx.say(filename..'\n')
--- end
--- pfile:close()
-
 local file = io.open(file_path, 'r')
 if (not file) then
     return ngx.exit(ngx.HTTP_NO_CONTENT)
